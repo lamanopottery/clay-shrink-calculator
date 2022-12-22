65fa37334c9e93ea8c93edbdd92e1b6d00847091
Alex Hadley <alexhad6@gmail.com>
2022-12-21 21:22:19 -0500

    Add Material UI and make initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { Box, TextField } from "@mui/material";
+
+const containerSx = {
+  display: "flex",
+  flexDirection: "column",
+  rowGap: 2,
+  maxWidth: "30rem",
+  margin: "auto",
+};
+
 export default function App() {
-  return <p>App</p>;
+  return (
+    <Box sx={containerSx} component="form">
+      <TextField type="number" label="Width" />
+      <TextField type="number" label="Height" />
+      <TextField type="number" label="Depth" />
+    </Box>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { createTheme, ScopedCssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#db3a20" },
+  },
+  typography: {
+    fontFamily: `"Open Sans", Verdana, Geneva, "DejaVu Sans", sans-serif`,
+  },
+});
 
 ReactDOM.createRoot(
   document.getElementById("clay-shrink-calculator") as HTMLElement,
 ).render(
   <StrictMode>
-    <App />
+    <ScopedCssBaseline>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ScopedCssBaseline>
   </StrictMode>,
 );
